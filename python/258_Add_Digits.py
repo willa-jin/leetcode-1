55fc7a28c2f2e9650ea9ad36d7d9de7fa5f594ab
willa-jin <willa.willa.jin@gmail.com>
2018-08-10 15:34:20 -0700

    Update 258_Add_Digits.py

--- python/258_Add_Digits.py
+++ python/258_Add_Digits.py
@@ -8,3 +8,15 @@ class Solution(object):
         if num < 10:
             return num
         return num - ((num - 1) / 9) * 9
+    
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        
+        while num >= 10:
+            num = sum(int(n) for n in str(num))
+            
+        return num
+    

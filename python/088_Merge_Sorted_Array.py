016b7e647e94b9260f61ba2f5e725a9d44b7f69e
willa-jin <willa.willa.jin@gmail.com>
2018-08-11 14:27:32 -0700

    Update 088_Merge_Sorted_Array.py

--- python/088_Merge_Sorted_Array.py
+++ python/088_Merge_Sorted_Array.py
@@ -7,6 +7,15 @@ class Solution(object):
         :type n: int
         :rtype: void Do not return anything, modify nums1 in-place instead.
         """
+        
+        # 直观思路显然是双指针i, j同时扫描A, B，选min(A[i], B[j])作为下一个元素插入。
+        #但是只能利用A后面的空间来插入，这样就很不方便了。
+        #反向思路，merge后的数组一共有m+n个数。
+        #i, j从A, B尾部扫描，选max(A[i], B[j])插入从m+n起的尾部。
+        #这样也可以防止插入到A原来数字的范围内时，overwrite掉A原来的数。
+
+        
+        
         p1, p2 = m - 1, n - 1
         pos = m + n - 1
         while p1 >= 0 and p2 >= 0:
@@ -17,10 +26,10 @@ class Solution(object):
                 nums1[pos] = nums2[p2]
                 p2 -= 1
             pos -= 1
-        while p2 >= 0:
-            nums1[pos] = nums2[p2]
-            p2 -= 1
-            pos -= 1
+            
+        if p2 >= 0:
+            nums1[: p2 + 1] = nums2[: p2 + 1]  # be careful with slicing here a[:n] = a[0 : n-1]
+
 
     # def merge(self, nums1, m, nums2, n):
     #     # using slicing

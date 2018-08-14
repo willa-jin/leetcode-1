070c456c886eb49756209d5f3072a60087acb875
willa-jin <willa.willa.jin@gmail.com>
2018-08-14 15:21:55 -0700

    Update 101_Symmetric_Tree.py

--- python/101_Symmetric_Tree.py
+++ python/101_Symmetric_Tree.py
@@ -5,7 +5,7 @@
 #         self.left = None
 #         self.right = None
 
-class Solution(object):
+class Solution(object):   ## D C
     def isSymmetric(self, root):
         """
         :type root: TreeNode
@@ -16,15 +16,47 @@ class Solution(object):
         return self.mirrorVisit(root.left, root.right)
 
     def mirrorVisit(self, left, right):
-        if left is None and right is None:
+        if not left and not right:
             return True
-        try:
-            if left.val == right.val:
-                if self.mirrorVisit(left.left, right.right) and self.mirrorVisit(left.right, right.left):
-                    return True
+        
+        if not left or not right:
             return False
-        except:
+        
+        if left.val != right.val:
             return False
+        
+        return self.mirrorVisit(left.left, right.right) and self.mirrorVisit(left.right, right.left)
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):   ## recursively
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """ 
+        # edge
+        if not root:
+            return True
+        # process
+        
+        def dfs(left, right):
+            if not left and not right:
+                return True
+            if not left or not right:
+                return False
+            if left.val != right.val:
+                return False
+            else:
+                return dfs(left.left, right.right) and (left.right, right.left)
+        
+        #recursion
+        return dfs(root.left, root.right)
         
         
       # stack
@@ -49,9 +81,9 @@ class Solution(object):
                 continue
             elif not left or not right:
                 return False
+            elif left.val != right.val:
+                return False
             elif left.val == right.val:
                 stack.insert(0, (left.left, right.right))
                 stack.insert(0, (left.right, right.left))
-            else:
-                return False
          return True
